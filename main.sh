f60b1fdeb8030123c97045f0a32b3b3ecca9766e
Hashir Rashid <hashir.rashid@ontariotechu.net>
2025-11-24 18:56:49 -0500

    Add users

--- main.sh
+++ main.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Load helper functions
+. ./functions.sh;
+
 # Top bound for introduction
 printf '=%.0s' {1..50};
 
 printf '\nSOFE 3200: Systems Programming, Group 15\n';
-printf 'Simple bash script to manage user accounts\n';
+printf 'Description: Simple bash script to manage user accounts\n';
 printf 'Usage: TEMP\n';
 
 # Bottom bound for introduction
@@ -16,13 +19,12 @@ printf 'Options: \n';
 printf '1: Add User\n'; 
 printf '2: Modify User\n'; 
 printf '3: Delete User\n'; 
-printf '4: List Wsers\n'; 
+printf '4: List All Users\n'; 
 printf '5: Exit\n'; 
 
 # Keep asking for input until a valid option is selected
 option=-1;
-while [ $option -lt 1 ] || [ $option -gt 6 ]; 
-do
+while [ $option -lt 1 ] || [ $option -gt 6 ]; do
 read -p "Enter an option: " option;
 
 if [ $option -lt 1 ] || [ $option -gt 6 ]; then
@@ -31,7 +33,25 @@ fi
 done
 
 # Option 1: Add User
+if [ $option -eq 1 ]; then
+    # Setup a trap in case an error occurs
+    trap 'echo "An error occurred. Ensure your password is at least 6 characters long. Exiting..."; exit 1;' ERR;
 
+    # Set variables
+    first_name='';
+    last_name='';
+    username='';
+    password='';
+
+    # Get name, username, and password from user
+    read -p "Enter your first and last name (with a space in between): " first_name last_name;
+    read -p "Enter a username: " username;
+    read -sp "Enter a password (minimum of 6 characters): " password;
+
+    # Add a user and set their password
+    sudo useradd -m -s /bin/bash "$username";
+    echo $username:$password | sudo chpasswd;
+fi
 
 # Option 2: Modify User
 
@@ -39,9 +59,14 @@ done
 # Option 3: Delete User
 
 
-# Option 4: List Users
+# Option 4: List Users (TEMPORARY)
+if [ $option -eq 4 ]; then
+    getent passwd;
+fi
 
 
 # Option 5: Exit
-printf 'Exiting...\n';
-exit 1;
+if [ $option -eq 5 ]; then
+    printf 'Exiting...\n';
+    exit 1;
+fi
